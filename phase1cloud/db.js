5e1d1253f496e9d365e50f6e9c77f611796d5f49
fneamonitaki <79602295+fneamonitaki@users.noreply.github.com>
2024-11-12 16:56:14 +0200

    Update port db.js

--- phase1cloud/db.js
+++ phase1cloud/db.js
@@ -4,17 +4,7 @@ const pool = new Pool({
     password: "20022002",
     database: "products_database",
     host: "localhost",
-    port: 5432
+    port: 5002
 });
-
-/*
-const ordersPool = new Pool({
-    user: "postgres",
-    password: "20022002",
-    database: "orders_database", // The new orders database
-    host: "localhost",
-    port: 5432,
-});
-*/
 
 module.exports = pool;
